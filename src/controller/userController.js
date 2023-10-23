a9b0418d2d13b209f0460ec460bd9c6053df566c
kabuga2015@1 <boazkhalifa@gmail.com>
2023-10-23 16:52:13 +0200

    change

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -24,6 +24,7 @@ export const userRegistration = async (req, res) => {
         res.status(201).json({ message: 'User registered successfully', user });
 
     } catch (error) {
+        console.log(error)
         res.status(500).json({ error: 'An error occurred while registering the user' });
     }
 }
