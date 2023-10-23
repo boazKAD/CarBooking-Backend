5851a0ab9d60f0f646870bd6ed648866e033106c
kabuga2015@1 <boazkhalifa@gmail.com>
2023-10-23 17:17:14 +0200

    new change

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -1,12 +1,12 @@
 import UserModel from "../model/userModel";
-import bcrypt from "bcrypt";
 import mongoose from "mongoose";
+
+import bcrypt from 'bcrypt';
 
 export const userRegistration = async (req, res) => {
     try {
-
         const saltRounds = 10;
-        const existingUser = await UserModel.findOne({ email: req.body.email })
+        const existingUser = await UserModel.findOne({ email: req.body.email });
         if (existingUser) {
             return res.status(409).json({ message: 'User already exists' });
         }
@@ -15,19 +15,19 @@ export const userRegistration = async (req, res) => {
         const newUser = await UserModel({
             phone: req.body.phone,
             name: req.body.name,
-            role:req.body.role,
+            role: req.body.role,
             email: req.body.email,
             password: hashedPassword
         });
 
         const user = await newUser.save();
         res.status(201).json({ message: 'User registered successfully', user });
-
     } catch (error) {
-        console.log(error)
+        console.log(error);
         res.status(500).json({ error: 'An error occurred while registering the user' });
     }
-}
+};
+
 
 export const getAllUser = async (req, res) => {
     try {
@@ -42,7 +42,7 @@ export const getUserById = async (req, res) => {
         const id = req.params.id
         if (!mongoose.Types.ObjectId.isValid(id)) {
             return res.status(400).json({ message: 'Invalid ID format' });
-          }
+        }
         const data = await UserModel.findOne({ _id: id });
         res.status(200).json({ message: ' retrive User  successfully', data });
     } catch (error) {
@@ -51,17 +51,17 @@ export const getUserById = async (req, res) => {
     }
 }
 
-export const updateUserById =async (req, res) => {
+export const updateUserById = async (req, res) => {
     try {
-        const id =req.params.id
-        const updates =req.body
+        const id = req.params.id
+        const updates = req.body
         if (!mongoose.Types.ObjectId.isValid(id)) {
             return res.status(400).json({ message: 'Invalid ID format' });
-          }
-        const data = await UserModel.findOneAndUpdate({_id:id},updates,{new:true})
+        }
+        const data = await UserModel.findOneAndUpdate({ _id: id }, updates, { new: true })
 
-        if (!data){
-            return res.status(404).json({message: "User Not Found "})
+        if (!data) {
+            return res.status(404).json({ message: "User Not Found " })
         }
         res.status(200).json({ message: 'User Updated successfully', data });
 
